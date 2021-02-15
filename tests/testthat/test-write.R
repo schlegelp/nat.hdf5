f12bdf30a3dda69f3cb1ecd9be50c4d34ead6c90
Philipp Schlegel <observing@web.de>
2021-02-15 14:17:00 +0000

    fix tests

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -2,7 +2,8 @@ test_that("test save raw neuron data", {
   # Test writing kcs20 to temporary file
   test_filename <- tempfile(fileext = ".h5")
   # Note that we have to force IDs here
-  write.neurons.hdf5(nat::kcs20, test_filename, overwrite.neurons=T, force.id=T)
+  write.neurons.hdf5(nat::kcs20, test_filename, overwrite.neurons=T,
+                     serialized=F, raw=T, force.id=T)
 
   # Try to read back in
   nl = read.neurons.hdf5(test_filename, read='dotprops,mesh,skeleton')
@@ -19,7 +20,8 @@ test_that("test save serialized neurons", {
   # Test writing kcs20 to temporary file
   test_filename <- tempfile(fileext = ".h5")
   # Note that we have to force IDs here
-  write.neurons.hdf5(nat::kcs20, test_filename, overwrite.neurons=T, force.id=T)
+  write.neurons.hdf5(nat::kcs20, test_filename, overwrite.neurons=T,
+                     serialized=T, raw=F, force.id=T)
 
   # Try to read back in
   nl = read.neurons.hdf5(test_filename, read='dotprops,mesh,skeleton')
